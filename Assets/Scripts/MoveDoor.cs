1540721ab22168847dda3bd173cceb494bd26402
xNoxiu <zuzannalaube13@gmail.com>
2025-02-20 23:33:13 +0100

    kitteyyy
    cała mapka wszystko siedzi

---
+++ Assets/Scripts/MoveDoor.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class MoveDoor : MonoBehaviour
+{
+    private Vector3 startPos; 
+    private Vector3 targetPos; 
+    private bool isOpen = false; 
+    public float moveSpeed = 2f; 
+
+    void Start()
+    {
+        startPos = transform.position;
+        targetPos = startPos + new Vector3(-0.8f, 0, 0);
+    }
+
+    void OnMouseDown()
+    {
+        if (!isOpen)
+        {
+            StartCoroutine(MoveDoorSmoothly(targetPos));
+        }
+        else
+        {
+            StartCoroutine(MoveDoorSmoothly(startPos)); 
+        }
+        isOpen = !isOpen;
+    }
+
+    IEnumerator MoveDoorSmoothly(Vector3 target)
+    {
+        float time = 0;
+        Vector3 start = transform.position;
+
+        while (time < 1)
+        {
+            transform.position = Vector3.Lerp(start, target, time);
+            time += Time.deltaTime * moveSpeed;
+            yield return null;
+        }
+
+        transform.position = target; 
+    }
+}
